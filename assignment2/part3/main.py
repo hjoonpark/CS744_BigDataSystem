f4e50c4bf2f656daaf086e0f3c9fb54d55777419
Joon Park <hjoonpark.us@gmail.com>
2022-10-09 02:13:01 -0500

    py

--- assignment2/part3/main.py
+++ assignment2/part3/main.py
@@ -37,11 +37,8 @@ def test_model(model, test_loader, criterion, logger):
     test_loss /= len(test_loader)
     logger.print("Test average={}, accuracy={}/{}={}".foramt(test_loss, correct, len(test_loader.dataset), 100*correct/len(test_load.dataset)))
             
-def train_model(model, epoch, input_data, target_data, optimizer, criterion):
-    # process group
-    group = dist.group.WORLD
-    group_size = args.num_nodes
-    
+def train_model(model, epoch, input_data, target_data, optimizer, criterion, group, group_size):
+
     # each batch is divided into processors (nodes), and averaged gradient sent back to each node for respective back-propagation
     # we first send the gradients of the 3 nodes to the root node, average them, and then send them to the 3 nodes respectively.
     
@@ -132,8 +129,11 @@ def main():
     optimizer = optim.SGD(model.parameters(), lr=0.1, momentum=0.9, weight_decay=0.0001)
 
 
-    # running training for one epoch
+    # process group
+    group = dist.group.WORLD
+    group_size = args.num_nodes
 
+    # running training for one epoch
     t0 = time.time()
     n_iter = 0
     for epoch in range(1):
@@ -141,7 +141,7 @@ def main():
         # we first send the gradients of the 3 nodes to the root node, average them, and then send them to the 3 nodes respectively.
         running_loss = 0
         for batch_idx, (input_data, target_data) in enumerate(train_loader):
-            loss = train_model(model, epoch, input_data, target_data, optimizer, criterion)
+            loss = train_model(model, epoch, input_data, target_data, optimizer, criterion, group, group_size)
 
             n_iter += 1
             running_loss += loss.item()
