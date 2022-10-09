10142cb96aea3452261b5e8ef8bf421056c30375
Joon Park <hjoonpark.us@gmail.com>
2022-10-09 03:29:32 -0500

    py

--- assignment2/main.py
+++ assignment2/main.py
@@ -3,6 +3,7 @@ import torch
 import json
 import copy
 import numpy as np
+import argparse
 from torchvision import datasets, transforms
 import torch.nn as nn
 import torch.nn.functional as F
@@ -10,42 +11,18 @@ import torch.optim as optim
 import logging
 import random
 import model as mdl
+import torch.distributed as dist
 import time
 import sys
-sys.path.append(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), ".."))
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from logger import Logger
 
 device = "cpu"
 torch.set_num_threads(4)
+
 batch_size = 256 # batch for one node
 
-def train_model(model, train_loader, optimizer, criterion, epoch):
-    """
-    model (torch.nn.module): The model created to train
-    train_loader (pytorch data loader): Training data loader
-    optimizer (optimizer.*): A instance of some sort of optimizer, usually SGD
-    criterion (nn.CrossEntropyLoss) : Loss function used to train the network
-    epoch (int): Current epoch number
-    """
-    running_loss = 0
-    # remember to exit the train loop at end of the epoch
-    for batch_idx, (data, target) in enumerate(train_loader):
-        # zero the parameter gradients
-        optimizer.zero_grad()
-
-        # forward + backward + optimize
-        outputs = model(data)
-        loss = criterion(outputs, target)
-        loss.backward()
-        optimizer.step()
-
-        running_loss += loss.item()
-        if batch_idx % 20 == 19:    # print every 20 mini-batches
-            print('epoch:', epoch, 'batch num:', batch_idx, 'loss:', running_loss/20)
-            running_loss = 0.0
-    return None
-
-def test_model(model, test_loader, criterion):
+def test_model(model, test_loader, criterion, logger):
     model.eval()
     test_loss = 0
     correct = 0
@@ -58,20 +35,60 @@ def test_model(model, test_loader, criterion):
             correct += pred.eq(target.view_as(pred)).sum().item()
 
     test_loss /= len(test_loader)
-    print('Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-            test_loss, correct, len(test_loader.dataset),
-            100. * correct / len(test_loader.dataset)))
+    logger.print("Test average={}, accuracy={}/{}={}".format(test_loss, correct, len(test_loader.dataset), 100*correct/len(test_loader.dataset)))
             
+def train_model(model, epoch, input_data, target_data, optimizer, criterion, group, group_size):
+    # each batch is divided into processors (nodes), and averaged gradient sent back to each node for respective back-propagation
+    # we first send the gradients of the 3 nodes to the root node, average them, and then send them to the 3 nodes respectively.
+    
+    # zero the parameter gradients
+    optimizer.zero_grad()
+
+    # forward + backward + optimize
+    outputs = model(input_data)
+    loss = criterion(outputs, target_data)
+    
+    # compute gradient
+    loss.backward() 
+
+    # back-propagate
+    optimizer.step()
+
+    return loss
 
 def main():
+    # python main.py --master-ip $ip_address$ --num-nodes 4 --rank $rank$
+    parser = argparse.ArgumentParser(description='Distributed PyTorch Training')
+    parser.add_argument('--master-ip', default='10.10.1.1', type=str, metavar='N',help='manual ip number', dest='master_ip')
+    parser.add_argument('--num-nodes', default=4, type=int, help='number of nodes for distributed training', dest='num_nodes')
+    parser.add_argument('--rank', default=0, type=int, help='node rank for distributed training')
+    args = parser.parse_args()
+    print("args: {}".format(args))
+
+    rank = args.rank
+    
     file_path = os.path.abspath(os.path.dirname(__file__))
     save_dir = os.path.join(file_path, "output")
     os.makedirs(save_dir, exist_ok=True)
     log_path = os.path.join(save_dir, "log_rank{}.txt".format(rank))
     logger = Logger(log_path)
     print("log_path={}".format(log_path))
-    
 
+    """
+    torch.distributed.init_process_group() parameters
+    - backend: 
+        - "gloo": Gloo (for CPU)
+        - "nccl": NCCL (for GPU)
+        - "mpi": for some special cases?
+    - init_method: URL specifying how to initialize the process group. Default is "env://"
+    - world_size: Number of processes participating in the job
+    - rank: Rank of the current process (it should be a number between 0 and world_size-1)
+    """
+    init_method = "tcp://{}:6666".format(args.master_ip)
+    print("init_method: {}".format(init_method))
+    dist.init_process_group(backend="gloo", init_method=init_method, world_size=args.num_nodes, rank=args.rank)
+
+    # preprocessing dataset
     normalize = transforms.Normalize(mean=[x/255.0 for x in [125.3, 123.0, 113.9]],
                                 std=[x/255.0 for x in [63.0, 62.1, 66.7]])
     transform_train = transforms.Compose([
@@ -79,20 +96,28 @@ def main():
             transforms.RandomHorizontalFlip(),
             transforms.ToTensor(),
             normalize,
-            ])
+        ])
 
     transform_test = transforms.Compose([
             transforms.ToTensor(),
             normalize])
-    training_set = datasets.CIFAR10(root="./data", train=True, download=True, transform=transform_train)
-    train_loader = torch.utils.data.DataLoader(training_set, num_workers=2, batch_size=batch_size, sampler=None, shuffle=True, pin_memory=True)
-    test_set = datasets.CIFAR10(root="./data", train=False, download=True, transform=transform_test)
+    
+    # load train set
+    train_set = datasets.CIFAR10(root="./data", train=True, download=True, transform=transform_train)
+    train_loader = torch.utils.data.DataLoader(train_set, num_workers=2, batch_size=batch_size, sampler=None, shuffle=True, pin_memory=True)
 
+    # load test set
+    test_set = datasets.CIFAR10(root="./data", train=False, download=True, transform=transform_test)
     test_loader = torch.utils.data.DataLoader(test_set, num_workers=2, batch_size=batch_size, shuffle=False, pin_memory=True)
-    training_criterion = torch.nn.CrossEntropyLoss().to(device)
+
+    print("train_set: {}, test_set: {}".format(len(train_set), len(test_set)))
+    criterion = torch.nn.CrossEntropyLoss().to(device)
 
     model = mdl.VGG11()
+    
+    # [PART 3] use pytorch's distributed functionality
     model.to(device)
+    model = nn.parallel.DistributedDataParallel(model)
     print(model)
     print("device: {}".format(device))
 
@@ -105,7 +130,6 @@ def main():
     # running training for one epoch
     t0 = time.time()
     n_iter = 0
-    
     for epoch in range(1):
         # each batch is divided into processors (nodes), and averaged gradient sent back to each node for respective back-propagation
         # we first send the gradients of the 3 nodes to the root node, average them, and then send them to the 3 nodes respectively.
@@ -126,7 +150,7 @@ def main():
     # train is over
 
     # test
-    test_model(model, test_loader, training_criterion)
+    test_model(model, test_loader, criterion, logger)
 
 if __name__ == "__main__":
     # [IMPORTANT] set seeds
---
+++ assignment2/run_all_parts.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+PART=$1 # 1, 2a, 2b, 3
+RANK=$2
+echo "RANK=${RANK}"
+
+# ip of the root
+MASTER_IP=10.10.1.1
+NUM_NODES=4
+
+# make output directories for each of the nodes
+# root node: 0
+cd ~ 
+# worker nodes: 1, 2, 3
+ROOT_DIR="/users/hpark376/CS744_BigDataSystem/assignment2/part3"
+mkdir -p ${ROOT_DIR}/output
+
+# use python from conda
+PYTHON_DIR="/users/hpark376/miniconda3/bin/"
+# CMD_PATH="export PATH=\"/users/hpark376/miniconda3/bin:\$PATH\""
+
+# run script for current node
+SCRIPT=${ROOT_DIR}/part${PART}/main.py
+echo "Running part: ${SCRIPT} with rank ${RANK}"
+python ${SCRIPT} --master-ip $MASTER_IP --num-nodes $NUM_NODES --rank ${RANK} #>> ${ROOT_DIR}/output/log_rank${RANK}.txt
+
+echo "==== DONE ===="
