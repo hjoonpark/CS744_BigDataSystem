8ef022e7e51f75ae86e2225cbf530046c10d3b77
Joon Park <hjoonpark.us@gmail.com>
2022-10-09 01:50:57 -0500

    py

--- assignment2/part3/main.py
+++ assignment2/part3/main.py
@@ -12,7 +12,8 @@ import logging
 import random
 import model as mdl
 import torch.distributed as dist
-from ...logger import Logger
+sys.path.insert(0,'..')
+from logger import Logger
 
 device = "cpu"
 torch.set_num_threads(4)
